b528cca3b5c85ec17049c0d06b8493ba3d54276d
Bram Borggreve <borggreve@gmail.com>
2022-05-08 20:19:45 +0100

    docs: add Kin Foundation to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,12 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const subtitle = 'By the Kin Foundation.'
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
+        <h4>{subtitle}</h4>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
