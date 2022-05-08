4489336c03e9712b491b394837cfd5fdd0bc39a1
Bram Borggreve <borggreve@gmail.com>
2022-05-08 20:35:04 +0100

    chore: fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,15 +36,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/kin-labs/mogami-io/tree/main/packages/create-mogami-io/templates/shared/',
+          editUrl: 'https://github.com/kin-labs/mogami-io/tree/main/',
         },
         // blog: {
         //   showReadingTime: true,
         //   // Please change this to your repo.
         //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/kin-labs/mogami-io/tree/main/packages/create-mogami-io/templates/shared/',
+        //   editUrl: 'https://github.com/kin-labs/mogami-io/tree/main/',
         // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
